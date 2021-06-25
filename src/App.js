fe354d0f754c0cf4753282a3992efa4cbe48da6f
Paweł <p.nowecki@gmail.com>
2021-06-25 23:16:49 +0200

    feat: fetch API by useEffect async function, handle data in data state. Done Change handler function and suggestion handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react'
 function App() {
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch('https://jsonplaceholder.typicode.com/users')
+      response = await response.json()
+      setData(response)
+    }
+    fetchMyAPI()
+  }, [])
+
+  console.log(data)
+
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length > 0) {
+      matches = data.filter(data => {
+        const regex = new RegExp(`${text}`, "gi");
+        return data.name.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    console.log(suggestions)
+    setSuggestions(matches)
+    setText(text)
+  }
+
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <input
+          type='text'
+          onChange={e => onChangeHandler(e.target.value)}
+          value={text}
+          placeholder='username'
+        />
+        {suggestions && suggestions.map((suggestions, i) =>
+          <div key={i} className='suggestion'
+            onClick={() => onSuggestHandler(suggestions.name)}
+          >
+            {suggestions.name}
+          </div>
+        )}
+      </main>
     </div>
   );
 }
